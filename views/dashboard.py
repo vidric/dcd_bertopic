49ad9c403d2fb035d14707ba0709e37b3ee64201
vidric <54578949+vidric@users.noreply.github.com>
2023-07-17 11:25:04 +0700

    Update dashboard.py

--- views/dashboard.py
+++ views/dashboard.py
@@ -161,7 +161,7 @@ class Dashboard:
                 df = db.query('SELECT * from produk')
                 df['nama'] = df.apply(lambda x: f'<a href="https://bertopic.streamlit.app/product/{x.id}">{x.nama}</a>', axis=1)
                 df = df.drop(columns=['id'])
-                df.set_index('nama', inplace=True)  # Ubah kolom 'nama' menjadi indeks
+                #df.set_index('nama', inplace=True)  # Ubah kolom 'nama' menjadi indeks
                 st.write(df.to_html(escape=False, index=True), unsafe_allow_html=True)
             
             with col2:
