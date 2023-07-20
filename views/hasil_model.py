4fa6e67a0c57904e36d81e44bab29589aa08fbb9
vidric <54578949+vidric@users.noreply.github.com>
2023-07-20 12:59:50 +0700

    Update hasil_model.py

--- views/hasil_model.py
+++ views/hasil_model.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from bertopic import BERTopic
+from conn import Database
 
 class HasilModel:
     class Model:
@@ -8,6 +9,7 @@ class HasilModel:
     def view(self, model):
 
         with st.container():
+            db = Database()
             st.markdown("<h1 style='text-align: center; color: black; font-weight: bold;'>Topic Modelling Customer Review Parfum HMNS Menggunakan BERTopic</h1>", unsafe_allow_html=True)
 
             df = db.query('SELECT review_asli AS Review, rating AS Rating from reviews WHERE LENGTH(review_asli) > 0 ORDER BY tanggal_convert DESC LIMIT 10')
