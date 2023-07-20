b2a2f8b39a712ad987e4d57f58c1ca7b3bb65919
vidric <54578949+vidric@users.noreply.github.com>
2023-07-20 12:58:30 +0700

    Update hasil_model.py

--- views/hasil_model.py
+++ views/hasil_model.py
@@ -10,10 +10,7 @@ class HasilModel:
         with st.container():
             st.markdown("<h1 style='text-align: center; color: black; font-weight: bold;'>Topic Modelling Customer Review Parfum HMNS Menggunakan BERTopic</h1>", unsafe_allow_html=True)
 
-        with st.container():
             df = db.query('SELECT review_asli AS Review, rating AS Rating from reviews WHERE LENGTH(review_asli) > 0 ORDER BY tanggal_convert DESC LIMIT 10')
             st.dataframe(df, width=1000, hide_index = True)
           
             st.markdown("---")
-
-
