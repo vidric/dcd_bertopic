f71b415e8d62a2c1ecbd71c1c21aadd89a3a96e5
vidric <54578949+vidric@users.noreply.github.com>
2023-08-31 00:49:34 +0700

    Update hasil_model.py

--- views/hasil_model.py
+++ views/hasil_model.py
@@ -42,14 +42,10 @@ class HasilModel:
                 selected_topics_str = ', '.join([f"'{topik}'" for topik in selected_topics])
                 st.write("#### Daftar Review")
                 query2 = f"""
-                        SELECT a.review_asli, a.review_cleansing, c.cluster, a.rating 
-                        FROM reviews a 
-                        LEFT JOIN review_topic b ON a.id = b.review_id 
-                        LEFT JOIN topics c ON c.id = b.topic_id 
-                        WHERE LENGTH(a.review_cleansing) > 10 
-                        AND c.cluster IS NOT NULL 
-                        AND c.cluster IN ({selected_topics_str}) 
-                        AND b.urutan IN ('4');
+                        SELECT b.review_asli, b.review_cleansing, a.cluster, b.rating 
+                        FROM result a 
+                        LEFT JOIN reviews b ON b.id = a.review_id 
+                        WHERE a.cluster IN ({selected_topics_str});
                         """
 
                 df = db.query(query2)
